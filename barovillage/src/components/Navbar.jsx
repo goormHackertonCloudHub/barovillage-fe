adb446538e9467cbdb7d2fb6ad7c7c7d3da9a1d8
seungineer <choseung97@gmail.com>
2024-12-15 08:27:38 +0900

    feat(Navbar): 글쓰기 버튼 추가

--- barovillage/src/components/Navbar.jsx
+++ barovillage/src/components/Navbar.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { useNavigate, useLocation } from "react-router-dom";
 import HOME_SYMBOL from "../assets/Home.png";
+import CREATEPOST_SYMBOL from "../assets/CreatePost.png";
 export default function Navbar() {
   const navigate = useNavigate();
   const location = useLocation();
 
   return (
     <nav className="bottom-0 left-0 right-0">
-      <div className="flex justify-around items-center h-14 border-t border-gray-200">
+      <div className="flex justify-around items-center p-3 h-14 border-t border-gray-200">
         <button
           onClick={() => navigate('/')}
           className={`flex-1 py-2 text-center ${
@@ -19,6 +20,20 @@ export default function Navbar() {
             <div className="flex flex-col items-center">
             <img src={HOME_SYMBOL} alt="홈" className="w-6 h-6" style={{ width: '18px', height: '18px' }} />
             <span className="text-[15px]">홈</span>
+        </div>
+        </button>
+        {/* 글쓰기 버튼 추가 */}
+        <button
+          onClick={() => navigate('/post/create')}
+          className={`flex-1 py-2 text-center ${
+            location.pathname === '/post/create'
+              ? 'text-blue-500 font-bold' 
+              : 'text-gray-500'
+          }`}
+        >
+          <div className="flex flex-col items-center">
+            <img src={CREATEPOST_SYMBOL} alt="글쓰기" className="w-6 h-6" style={{ width: '18px', height: '18px' }} />
+            <span className="text-[15px]">글쓰기</span>
         </div>
         </button>
       </div>
